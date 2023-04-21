4ad0bd212637793da0c12156e80da8e7eecf3f60
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-21 15:40:34 +0200

    Show selected item while changing it's color

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { DisplayDifficulty } from "./components/DisplayDifficulty/DisplayDifficulty";
 import { MenuList } from "./components/MenuList/MenuList";
-import { MenuListItem } from "./components/MenuListItem/MenuListItem";
+import { useState } from "react";
 import s from "./style.module.css";
 
 export function App() {
+  const [currentDifficulty, setCurrentDifficulty] = useState("Insane");
+
+  const onMenuListItemClick = (difficulty) => {
+    setCurrentDifficulty(difficulty);
+  };
+
   return (
     <div>
-      <h1>Select your React difficulty</h1>
+      <h1 style={{ textAlign: "center" }}>Select your React difficulty</h1>
       <div className={s.centerElems}>
-        <MenuList />
-        <DisplayDifficulty difficulty="Low" />
+        <MenuList
+          onItemClick={onMenuListItemClick}
+          difficulty={currentDifficulty}
+        />
+        <DisplayDifficulty difficulty={currentDifficulty} />
       </div>
     </div>
   );
--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,13 +1,29 @@
 import { MenuListItem } from "../MenuListItem/MenuListItem";
 import s from "./style.module.css";
 
-export function MenuList() {
+export function MenuList(props) {
   return (
     <div className={s.container}>
-      <MenuListItem difficulty="Low 🟢" />
-      <MenuListItem difficulty="Medium 🟡" />
-      <MenuListItem difficulty="High 🔴" />
-      <MenuListItem difficulty="Insane ⚫️" />
+      <MenuListItem
+        isSelected={props.difficulty === "Low"}
+        onClick={props.onItemClick}
+        difficulty="Low"
+      />
+      <MenuListItem
+        isSelected={props.difficulty === "Medium"}
+        onClick={props.onItemClick}
+        difficulty="Medium"
+      />
+      <MenuListItem
+        isSelected={props.difficulty === "Hard"}
+        onClick={props.onItemClick}
+        difficulty="High"
+      />
+      <MenuListItem
+        isSelected={props.difficulty === "Insane"}
+        onClick={props.onItemClick}
+        difficulty="Insane"
+      />
     </div>
   );
 }
